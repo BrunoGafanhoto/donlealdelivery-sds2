69e9b2913520919957237f3327844385450e73cb
Bruno dos Santos <projetointegrador792@gmail.com>
2021-01-07 20:02:31 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/donlealdelivery/DonlealdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.donlealdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonlealdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonlealdeliveryApplication.class, args);
+	}
+
+}
