bb5d3841752b9b25b7792a22ef9382f66412e01e
Bruno dos Santos <projetointegrador792@gmail.com>
2021-01-08 13:27:52 -0300

    GET Orders

--- backend/src/main/java/com/devsuperior/donlealdelivery/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/donlealdelivery/repositories/OrderRepository.java
@@ -1,11 +1,17 @@
 package com.devsuperior.donlealdelivery.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
 import com.devsuperior.donlealdelivery.entities.Order;
 
 public interface OrderRepository extends JpaRepositoryImplementation<Order, Long> {
 	
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List<Order> findOrdersWithProducts();
 }
 
 
