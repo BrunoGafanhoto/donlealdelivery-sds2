ee80d3868966a481791b4565488c930514e32a4e
Bruno dos Santos <projetointegrador792@gmail.com>
2021-01-10 02:17:19 -0300

    Add NavBar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
 
 
 function App() {
   return (
 
-    <div className="">
-      
-     Hello!
+    <div>
+     
+     <Navbar />
       
     </div>
   );
