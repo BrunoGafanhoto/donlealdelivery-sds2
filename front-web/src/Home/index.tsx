0427e9d80c0bf68ee9f297d42a37dd7775c651b6
Bruno dos Santos <projetointegrador792@gmail.com>
2021-01-11 03:27:14 -0300

    ajustes

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -9,7 +9,7 @@ function Home() {
                 <div className="home-content">
                     <div className="home-actions">
                         <h1 className="home-title">
-                            Faça seu pedido<br />que entregamos<br />pra você!!!
+                            Faça seu pedido<br />que entregamos<br />pra você!
                 </h1>
                         <h3 className="home-subtitle">
                             Escolha seu pedido e em poucos minutos <br />levaremos na sua porta
