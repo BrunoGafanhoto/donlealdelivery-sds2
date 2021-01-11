d9bcc7a530229842bcac7907fd24a2ef187a0aea
Bruno dos Santos <projetointegrador792@gmail.com>
2021-01-11 00:08:56 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,17 @@
-import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Home from './Home';
 import Navbar from './Navbar';
 import Routes from './Routes';
-
+import './App.css';
 
 function App() {
   return (
-
+    <>
     <Routes/>
+    <ToastContainer/>
+    </>
   );
 }
 
